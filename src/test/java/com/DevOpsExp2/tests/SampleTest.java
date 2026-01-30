522ecf7351f1dbdecbdb46831b730d4c2af123d6
narendra5689 <nellurinarendra4@gmail.com>
2026-01-30 20:11:48 +0530

    ntial commit

---
+++ src/test/java/com/DevOpsExp2/tests/SampleTest.java
@@ -0,0 +1,11 @@
+package com.DevOpsExp2.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void demoTest() {
+        System.out.println("TestNG is working successfully");
+    }
+}
